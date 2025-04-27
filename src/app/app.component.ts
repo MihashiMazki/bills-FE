7bd1994bf94a7c4d4349bc6283dd0666fd435ac5
test <mohdhisyamudin.b@tcs.com>
2025-04-27 15:53:50 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    RippleModule,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    InputTextModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'hisyam-bills';
